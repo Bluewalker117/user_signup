1a4b8fc6e87d9fbb1e45294d2ce7e03bf04e4b8e
Robert Nansteel <imblueman2@yahoo.com>
2018-04-08 14:17:45 -0500

    User Name Errors Working

--- main.py
+++ main.py
@@ -10,15 +10,46 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True  
 
+
+def valid_user_name(name):    
+    if 2 < len(name) < 21:
+        return True
+    else:
+        return False 
+
 @app.route("/input", methods=["POST"])
-def confirm():
-    user= request.form["user_name"]
-    template=jinja_env.get_template("confirm.html")
-    return template.render(user_name = user)
+def test():
+    user = request.form["user_name"]
+    user_name_error = "Ooooops!"
+    if user.strip()=="":
+        return "Please enter a user name."
+    if valid_user_name(user) == True:
+        return "success"
+    else:
+        return "The name you have entered is not the required 3-20 characters in length.  Please try a new name."
+
+#@app.route("/input", methods=["POST"])
+#def input():
+#    user= request.form["user_name"]
+#    if user.strip()=="":
+#        error="Please enter a user name."
+#        return redirect("/?error" + error)
+
+
+ #   template=jinja_env.get_template("confirm.html")
+  #  return template.render(user_name = user)
+
+@app.route("/error", methods=["GET"])
+def error():
+    return "<p>Error.</p>"
+
+
+
 
 @app.route("/")
 def index():
-    return render_template("input.html")
+    show_error = request.args.get("error")
+    return render_template("input.html", error=show_error)
 
 
 app.run() 
